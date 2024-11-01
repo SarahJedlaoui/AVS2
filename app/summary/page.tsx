c5cc8d6199b1d04d29e643656a9e30ae8a3b777e
SarahJedlaoui <sarajedlaoui999@gmail.com>
2024-11-01 10:56:21 +0100

    commit

--- app/summary/page.tsx
+++ app/summary/page.tsx
@@ -1,32 +1,31 @@
 import {
-    Navbar,
-    Footer,
-    Summary
-  } from "../components";
-  import React, { Suspense } from "react";
-  
-  export default function Home() {
-    return (
-      <main className=" bg-primary w-full overflow-hidden font-poppins">
-        <header className="paddingX flexCenter">
-          <nav className="boxWidth">
-            <Navbar />
-          </nav>
-        </header>
-        <section className=" bg-primary flexStart ">
-          <section className="boxWidth">
+  Navbar,
+  Footer,
+  Summary
+} from "../components";
+import React, { Suspense } from "react";
+
+export default function Home() {
+  return (
+    <main className=" bg-primary w-full overflow-hidden font-poppins">
+      <header className="paddingX flexCenter">
+        <nav className="boxWidth">
+          <Navbar />
+        </nav>
+      </header>
+      <section className=" bg-primary flexStart ">
+        <section className="boxWidth">
           <Suspense fallback={<div>Loading...</div>}>
-    <Summary />
-  </Suspense>
-          </section>
+            <Summary />
+          </Suspense>
         </section>
-        <section className=" bg-primary paddingX flexStart">
-          <section className="boxWidth">
-           
-            <Footer />
-          </section>
+      </section>
+      <section className=" bg-primary paddingX flexStart">
+        <section className="boxWidth">
+
+          <Footer />
         </section>
-      </main>
-    );
-  }
-  
+      </section>
+    </main>
+  );
+}
