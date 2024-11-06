2c2b8ca13ed7e8ab78e0ef338a84b15dd7d8a6e2
SarahJedlaoui <sarajedlaoui999@gmail.com>
2024-11-06 15:51:54 +0100

    make it better

--- app/components/Sammary.tsx
+++ app/components/Sammary.tsx
@@ -129,7 +129,7 @@ const Summary: React.FC = () => {
             <button
               key={section}
               className={clsx(
-                "px-4 py-2 rounded-full font-medium",
+                "px-2 py-1 rounded-full text-md font-medium whitespace-nowrap",
                 filter === section ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-700"
               )}
               onClick={() => setFilter(section)}
