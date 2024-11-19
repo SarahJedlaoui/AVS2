b0abaa9f845ff0f0e88f2e9e88f950e4be9d41e9
SarahJedlaoui <sarajedlaoui999@gmail.com>
2024-11-19 15:36:10 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "AVS",
-  description: "The Next Generation Banking",
+  description: "After Visit Summary",
 };
 
 export default function RootLayout({
